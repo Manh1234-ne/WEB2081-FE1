5f09bcc38b9b92bd5a49fd11d055d46f4664f828
Manh1234-ne <manhnon16@gmail.com>
2026-03-12 12:21:51 +0700

    cach routing angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home', // Ko thêm / vào đầu đường dẫn
+        component: Home,
+    }
+];
